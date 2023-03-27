67f46052a95f2895f1fee4bb97a50232820221de
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-03-27 17:48:29 -0300

    Setup routes page to format false

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Vitepress::Engine.routes.draw do
-  get "/:id", to: "pages#show", as: :page
+  get "/*id", to: "pages#show", as: :page, format: false
   root to: "pages#index"
 end
