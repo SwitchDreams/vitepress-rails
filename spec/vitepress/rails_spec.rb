265e50d276e0b98c6a92b2018817b82d50d5f3f2
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-03-27 11:14:09 -0300

    Install switchcop

--- spec/vitepress/rails_spec.rb
+++ spec/vitepress/rails_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe Vitepress::Rails do
   it "has a version number" do
-    expect(Vitepress::Rails::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(Vitepress::Rails::VERSION).not_to be_nil
   end
 end
