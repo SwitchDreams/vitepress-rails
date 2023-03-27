9495fa8bfd5d452d998f5f491ad4437419ec09b7
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2023-03-27 11:09:59 -0300

    Initialize vitepress-rails

---
+++ lib/vitepress/rails/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Vitepress
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/vitepress/rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vitepress::Rails do
+  it "has a version number" do
+    expect(Vitepress::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
